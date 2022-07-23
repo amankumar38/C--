e300bfd68e2ed8f530ffe517e9e392cf99488a49
amankumar38 <amankumar32.ak@gmail.com>
2022-07-23 20:33:38 +0530

    update

---
+++ cpp_functions/fun.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void myfunc()
+{
+    cout << "Hello i am the function you made. " << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    myfunc();
+    myfunc();
+    myfunc();
+    myfunc();
+    myfunc();
+    return 0;
+}
