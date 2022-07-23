de583e10c74dab8271c4e59c8f8658eb48a47d06
amankumar38 <amankumar32.ak@gmail.com>
2022-07-23 21:41:07 +0530

    update

--- cpp_functions/with_perameter/fun_perameter.cpp
+++ cpp_functions/with_perameter/fun_perameter.cpp
@@ -8,11 +8,21 @@ void fun_pera(string fname)
     cout << fname << " S" <<endl;
 }
 
+//Defult perameter
+void fun_pera_default(string country = "India")
+{
+    cout << country << endl;
+}
+
 int main(int argc, char const *argv[])
 {
     fun_pera("A");
     fun_pera("M");
     fun_pera("T");
     fun_pera("K");
+
+    fun_pera_default();
+    fun_pera_default("Norway");
+    fun_pera_default("Sweden");
     return 0;
 }
