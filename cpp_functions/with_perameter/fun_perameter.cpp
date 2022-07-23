6b5ea2119b1fc4d14e80eb16da02142c8c44aa54
amankumar38 <amankumar32.ak@gmail.com>
2022-07-23 21:48:39 +0530

    update

--- cpp_functions/with_perameter/fun_perameter.cpp
+++ cpp_functions/with_perameter/fun_perameter.cpp
@@ -14,6 +14,12 @@ void fun_pera_default(string country = "India")
     cout << country << endl;
 }
 
+// multiple perameters
+void fun_pera_multi(string fname, int age)
+{
+    cout << fname << " k " << age << " is years age old. " <<endl;
+}
+
 int main(int argc, char const *argv[])
 {
     fun_pera("A");
@@ -24,5 +30,9 @@ int main(int argc, char const *argv[])
     fun_pera_default();
     fun_pera_default("Norway");
     fun_pera_default("Sweden");
+
+    fun_pera_multi("A " , 25);
+    fun_pera_multi("M " , 22);
+    fun_pera_multi("T ", 16);
     return 0;
 }
