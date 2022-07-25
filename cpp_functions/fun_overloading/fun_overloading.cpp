6bf2cc8c7e4fa473c737923417a2251464fa7f62
amankumar38 <amankumar32.ak@gmail.com>
2022-07-25 23:11:18 +0530

    update

--- cpp_functions/fun_overloading/fun_overloading.cpp
+++ cpp_functions/fun_overloading/fun_overloading.cpp
@@ -14,7 +14,6 @@ double myfun(double x , double y)
 
 int main(int argc, char const *argv[])
 {
-    // input();
     
     std::cout << "Int: " << myfun(3, 4) << std::endl;
     std::cout << "Double: " << myfun(3.4, 4.5) << std::endl;
