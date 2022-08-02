3916f17618653447804e49d30c787dd90efead86
amankumar38 <amankumar32.ak@gmail.com>
2022-08-03 00:00:53 +0530

    Cpp practice code

---
+++ arthmetic/average/average_two_num.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+class average{
+public:
+    int frist_number, second_number , sum;
+    float average;
+
+void values()
+{
+    std::cout << "Enter the Frist number: " << std::endl;
+    std::cin >> frist_number;
+
+    std::cout << "Enter the second number: " << std::endl;
+    std::cin >> second_number;
+}
+
+void average_of_two_numbers()
+{
+
+    sum = frist_number + second_number;
+    average = sum / 2;
+
+    std::cout << "Average of Two numbers is : " << average <<std::endl;
+}
+};
+
+int main(int argc, char const *argv[])
+{
+    average myAverage;
+    
+    myAverage.values();
+
+    myAverage.average_of_two_numbers();
+
+    return 0;
+}
