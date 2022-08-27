9a60dc01d680824a6cd31d9302da237af165ae23
amankumar38 <amankumar32.ak@gmail.com>
2022-08-27 18:38:25 +0530

    Cpp Practice code

---
+++ array/sum of 10 numbers/sum_using_array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void sum()
+{
+    int arr[10], i, j, sum_of_elements;
+    float average;
+    std::cout << "Enter the element of array: "<< std::endl;
+    for(i = 1; i <=10; i++)
+    {
+        arr[i] = rand();
+    }
+    std::cout << std::endl;
+
+    for(j = 1; j <=10; j++)
+    {
+        std::cout  <<" " << arr[j];
+
+        sum_of_elements += arr[j];
+    }
+    std::cout << std::endl;
+
+    std::cout << "sum = " << sum_of_elements << std::endl;
+
+    average = sum_of_elements / 2;
+
+    std::cout << "average = " << average;
+
+    
+}
+
+int main()
+{
+    sum();
+    return 0;
+}
